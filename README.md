94fb2d25af37a0224fa8e117793c1104e8a6eee9
Gabriel Melo <115416454+GabriielMelo@users.noreply.github.com>
2023-08-02 21:26:53 -0300

    feat: criado app
    iniciado app com criação dos cards

--- README.md
+++
@@ -1,2 +0,0 @@
-# Copa Feminina 2023
- Projeto em react js para acompanhar a copa mundial feminina de 2023
---
+++ copa2023/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './components/Card'
+function App() {
+
+  return (
+    <>
+      <h1>Copa do Mundo Feminina 2023</h1>
+      <section className="cards">
+        <Card />
+      </section>
+    </>
+  )
+}
+
+export default App
